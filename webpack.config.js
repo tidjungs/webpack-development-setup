ef8799fd617b233ae2a8bc5e4b78e35cc292e772
tidjungs <waewprach.s@ku.th>
2017-06-05 08:43:23 +0700

    config webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const webpack = require('webpack')
 
 module.exports = {
   devtool: 'eval-source-map',
@@ -48,5 +49,16 @@ module.exports = {
         ]
       }
     ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    hot: true,
+    historyApiFallback: true,
+    proxy: {
+      '/api': 'http://localhost:3000'
+    }
   }
 }
