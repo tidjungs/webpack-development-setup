0e95d4ad936eadc827c2153aa57b264595c688d2
tidjungs <waewprach.s@ku.th>
2017-06-05 08:55:13 +0700

    add eslint

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,7 @@ module.exports = {
   },
   module: {
     rules: [
+      { test: /\.js$/, exclude: /node_modules/, enforce: 'pre', loader: 'eslint-loader' },
       { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
       {
         test: /\.scss$/,
